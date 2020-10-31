79ae2170c8a72623d0f2aaa3ef2478d015af3078
sparcopt <j.l.p.almeida42@gmail.com>
2020-10-31 19:19:20 +0000

    Add enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject ZombiePrefab;
+    [HideInInspector]
+    public Transform[] EnemySpawnPoints;
+    public float SpawnDuration = 5f;
+
+    void Start()
+    {
+        EnemySpawnPoints = new Transform[transform.childCount];
+
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            EnemySpawnPoints[i] = transform.GetChild(i);
+        }
+
+        StartCoroutine(CoStartSpawning());
+    }
+
+    private IEnumerator CoStartSpawning()
+    {
+        while(true)
+        {
+            for(int i = 0; i < EnemySpawnPoints.Length; i++)
+            {
+                Instantiate(ZombiePrefab, EnemySpawnPoints[i].position, EnemySpawnPoints[i].rotation);
+            }
+
+            yield return new WaitForSeconds(SpawnDuration);
+        }
+    }
+}
